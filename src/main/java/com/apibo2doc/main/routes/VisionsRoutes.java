e3a93c84176e489d22d63f3d19a498bb4ca3409f
jcsalinas20 <jcsalinasn20@gmail.com>
2021-10-28 20:26:02 +0200

    -Remove imports

--- src/main/java/com/apibo2doc/main/routes/VisionsRoutes.java
+++ src/main/java/com/apibo2doc/main/routes/VisionsRoutes.java
@@ -1,9 +1,7 @@
 package com.apibo2doc.main.routes;
 
 import com.apibo2doc.main.controllers.VisionsController;
-import com.apibo2doc.main.controllers.WeaponsController;
 import com.apibo2doc.main.models.VisionsModel;
-import com.apibo2doc.main.models.WeaponsModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;

21221e0932ad2749c0ac38d864ff4b38c4f64d69
jcsalinas20 <jcsalinasn20@gmail.com>
2021-10-28 20:35:16 +0200

    -List all vehicles -List custom vehicles

---
+++ src/main/java/com/apibo2doc/main/routes/VehiclesRoutes.java
@@ -0,0 +1,43 @@
+package com.apibo2doc.main.routes;
+
+import com.apibo2doc.main.controllers.VehiclesController;
+import com.apibo2doc.main.controllers.VisionsController;
+import com.apibo2doc.main.models.VehiclesModel;
+import com.apibo2doc.main.models.VisionsModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/vehicles")
+public class VehiclesRoutes {
+
+    @GetMapping()
+    public VehiclesModel getAllVehicles() {
+        List<String> weaponList = VehiclesController.getAllVehicles();
+
+        VehiclesModel response = new VehiclesModel();
+        response.setStatus(weaponList.size() > 0);
+        response.setMessage("List of vehicles");
+        response.setCount(weaponList.size());
+        response.setList(weaponList);
+
+        return response;
+    }
+
+    @GetMapping("/query")
+    public VehiclesModel getVehicles(@RequestParam("name") String name) {
+        List<String> weaponList = VehiclesController.getCustomList(name.toLowerCase());
+
+        VehiclesModel response = new VehiclesModel();
+        response.setStatus(weaponList.size() > 0);
+        response.setMessage("List of vehicles");
+        response.setCount(weaponList.size());
+        response.setList(weaponList);
+
+        return response;
+    }
+}

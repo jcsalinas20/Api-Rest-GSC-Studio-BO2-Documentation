ab12ffef09c7918c21c94c0aa618c294ece0f12c
jcsalinas20 <jcsalinasn20@gmail.com>
2021-10-28 20:24:53 +0200

    -List all visions -List custom visions

---
+++ src/main/java/com/apibo2doc/main/models/VisionsModel.java
@@ -0,0 +1,45 @@
+package com.apibo2doc.main.models;
+
+import java.util.List;
+
+public class VisionsModel {
+
+    private Boolean status;
+    private String message;
+    private int count;
+    private List list;
+
+    public VisionsModel(){
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public List getList() {
+        return list;
+    }
+
+    public void setList(List list) {
+        this.list = list;
+    }
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
---
+++ src/main/java/com/apibo2doc/main/routes/VisionsRoutes.java
@@ -0,0 +1,43 @@
+package com.apibo2doc.main.routes;
+
+import com.apibo2doc.main.controllers.VisionsController;
+import com.apibo2doc.main.controllers.WeaponsController;
+import com.apibo2doc.main.models.VisionsModel;
+import com.apibo2doc.main.models.WeaponsModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/visions")
+public class VisionsRoutes {
+
+    @GetMapping()
+    public VisionsModel getAllVisions() {
+        List<String> weaponList = VisionsController.getAllVisions();
+
+        VisionsModel response = new VisionsModel();
+        response.setStatus(weaponList.size() > 0);
+        response.setMessage("List of visions");
+        response.setCount(weaponList.size());
+        response.setList(weaponList);
+
+        return response;
+    }
+
+    @GetMapping("/query")
+    public VisionsModel getVisions(@RequestParam("name") String name) {
+        List<String> weaponList = VisionsController.getCustomList(name.toLowerCase());
+
+        VisionsModel response = new VisionsModel();
+        response.setStatus(weaponList.size() > 0);
+        response.setMessage("List of visions");
+        response.setCount(weaponList.size());
+        response.setList(weaponList);
+
+        return response;
+    }
+}

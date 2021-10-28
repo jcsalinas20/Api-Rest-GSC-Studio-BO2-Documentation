42c18a2596167b0ecdfe472a9b0475f7630bffe3
jcsalinas20 <jcsalinasn20@gmail.com>
2021-10-28 12:51:40 +0200

    initialize repo

---
+++ src/main/java/com/apibo2doc/main/Index.java
@@ -0,0 +1,13 @@
+package com.apibo2doc.main;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Index {
+
+    @RequestMapping("/")
+    public String hello() {
+        return "Hola";
+    }
+}
---
+++ src/test/java/com/apibo2doc/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apibo2doc.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

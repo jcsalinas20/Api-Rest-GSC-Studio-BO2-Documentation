6b9613a6a7791fe15bd1517859701aea747681c0
jcsalinas20 <jcsalinasn20@gmail.com>
2021-10-30 23:27:16 +0200

    -Add new file for index page -List all the end points in the index.html

--- src/main/java/com/apibo2doc/main/Index.java
+++ src/main/java/com/apibo2doc/main/Index.java
@@ -3,11 +3,45 @@ package com.apibo2doc.main;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 @RestController
 public class Index {
 
     @RequestMapping("/")
-    public String hello() {
-        return "<a href='/weapons'>Weapons</a>";
+    public String mainPage() {
+        InputStream file = getFileAsIOStream("index.html");
+        return printFileContent(file);
+    }
+
+
+    private InputStream getFileAsIOStream(final String fileName) {
+        InputStream ioStream = this.getClass()
+                .getClassLoader()
+                .getResourceAsStream(fileName);
+
+        if (ioStream == null) {
+            throw new IllegalArgumentException(fileName + " is not found");
+        }
+        return ioStream;
+    }
+
+    private String printFileContent(InputStream file) {
+        String fileText = "";
+        try (InputStreamReader isr = new InputStreamReader(file);
+             BufferedReader br = new BufferedReader(isr);)
+        {
+            String line;
+            while ((line = br.readLine()) != null) {
+                fileText += line;
+            }
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return fileText;
     }
 }
